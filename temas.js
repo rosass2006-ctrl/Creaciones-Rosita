5b2b6113cea371454060c67279ef45c7bac62f0e
rosass2006-ctrl <rosass2006@gmail.com>
2025-11-21 12:34:29 -0400

    Add files via upload

---
+++ temas.js
@@ -0,0 +1,11 @@
+function cambiarTema(modo) {
+    if (modo === 'dia') {
+        document.body.style.background = '#fffefc';
+    }
+    if (modo === 'noche') {
+        document.body.style.background = '#1c1b1b';
+    }
+    if (modo === 'daltonico') {
+        document.body.style.background = '#f4f400';
+    }
+}
